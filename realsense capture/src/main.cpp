48361d6c7dab8f5bdab1933057285e94d0f6a840
Zhang Yuan <zhangyuan@ict.ac.cn>
2017-12-13 10:08:17 +0800

    Update 1213, resize capture image to 256 * 256.

--- realsense capture/src/main.cpp
+++ realsense capture/src/main.cpp
@@ -37,20 +37,21 @@ bool initialize_streaming()
     return success;
 }
 
+
 static void onMouse(int event, int x, int y, int, void* window_name)
 {
     if(event == cv::EVENT_LBUTTONDOWN)
     {
         _loop = false;
     }
-    if(event == cv::EVENT_RBUTTONDOWN)
+    /*if(event == cv::EVENT_RBUTTONDOWN)
     {
         _capture = true;
     }
     if(event == cv::EVENT_RBUTTONDBLCLK)
     {
         _capture = false;
-    }
+    }*/
 }
 
 void setup_windows()
@@ -67,14 +68,35 @@ bool display_next_frame()
     _depth_intrin = _rs_camera.get_stream_intrinsics(rs::stream::depth);
     _color_intrin = _rs_camera.get_stream_intrinsics(rs::stream::color);
 
+    cv::Point point_a = cv::Point(79, 0);
+    cv::Point point_b = cv::Point(559, 479);
+
     //Create depth image
     cv::Mat depth16(_depth_intrin.height, _depth_intrin.width, CV_16U, (uchar *)_rs_camera.get_frame_data(rs::stream::depth_aligned_to_color));
     cv::Mat rgb(_color_intrin.height, _color_intrin.width, CV_8UC3, (uchar *)_rs_camera.get_frame_data(rs::stream::color));
 
+    if(_capture)
+    {
+        stringstream sstream;
+        string str;
+        sstream << cur_frame;
+        sstream >> str;
+
+        cv::Rect rect(79, 0, 480, 480);
+        cv::Mat mid_image = depth16(rect);
+        cv::Mat save_image;
+        cv::resize(mid_image, save_image, cv::Size(256, 256));
+
+        string depth_file = "/Users/manster/Desktop/realsense optim/realsense capture/img/" + str + "_distance.png";
+        cv::imwrite(depth_file, save_image);
+    }
+
     cv::Mat depth8u = depth16;
     depth8u.convertTo(depth8u, CV_8UC1, 255.0 / 1000);
 
+    cv::rectangle(depth8u, point_a, point_b, cv::Scalar(255, 255, 0));
     cv::imshow(WINDOW_DEPTH, depth8u);
+
     if(_capture)
     {
         stringstream sstream;
@@ -82,13 +104,20 @@ bool display_next_frame()
         sstream << cur_frame;
         sstream >> str;
 
-        string depth_file = "/Users/manster/Desktop/graduate/realsense optim/realsense capture/img/" + str + "d.jpg";
-        cv::imwrite(depth_file, depth8u);
+        cv::Rect rect(79, 0, 480, 480);
+        cv::Mat mid_image = depth8u(rect);
+        cv::Mat save_image;
+        cv::resize(mid_image, save_image, cv::Size(256, 256));
+
+        string depth_file = "/Users/manster/Desktop/realsense optim/realsense capture/img/" + str + "_d.jpg";
+        cv::imwrite(depth_file, save_image);
     }
-    cvWaitKey(1);
+    //cvWaitKey(1);
 
     cv::cvtColor(rgb, rgb, cv::COLOR_BGR2RGB);
+    cv::rectangle(rgb, point_a, point_b, cv::Scalar(255, 255, 0));
     cv::imshow(WINDOW_RGB, rgb);
+
     if(_capture)
     {
         stringstream sstream;
@@ -96,10 +125,15 @@ bool display_next_frame()
         sstream << cur_frame;
         sstream >> str;
 
-        string color_file = "/Users/manster/Desktop/graduate/realsense optim/realsense capture/img/" + str + "c.jpg";
-        cv::imwrite(color_file, rgb);
+        cv::Rect rect(79, 0, 480, 480);
+        cv::Mat mid_image = rgb(rect);
+        cv::Mat save_image;
+        cv::resize(mid_image, save_image, cv::Size(256, 256));
+
+        string color_file = "/Users/manster/Desktop/realsense optim/realsense capture/img/" + str + "_c.jpg";
+        cv::imwrite(color_file, save_image);
     }
-    cvWaitKey(1);
+    //cvWaitKey(1);
 
     cur_frame += 1;
 
@@ -113,9 +147,9 @@ float getDistance(int x, int y)
     rs::intrinsics depthIntrin = _rs_camera.get_stream_intrinsics(rs::stream::depth_aligned_to_color);
     uint16_t depthValue = depthImage[y * depthIntrin.width + x];
     float depthMeters = depthValue * scale;
-    std::cout<<"Get Distance: scale is: "<<scale<<std::endl;
-    std::cout<<"Get Distance: depthValue is: "<<depthValue<<std::endl;
-    std::cout<<"Get Distance: depthMeters is: "<<depthMeters<<std::endl;
+    //std::cout<<"Get Distance: scale is: "<<scale<<std::endl;
+    //std::cout<<"Get Distance: depthValue is: "<<depthValue<<std::endl;
+    //std::cout<<"Get Distance: depthMeters is: "<<depthMeters<<std::endl;
     return depthMeters;
 }
 
@@ -132,10 +166,28 @@ int main() try
 
     setup_windows();
 
+    char keyboard_event = 'a';
+
+    float scale = _rs_camera.get_depth_scale();
+    cout<<"Scale is: "<<scale<<endl;
+
     while(_loop)
     {
         if(_rs_camera.is_streaming())
             _rs_camera.wait_for_frames();
+
+        keyboard_event = cvWaitKey(1);
+
+        if(keyboard_event == 's')
+        {
+            cout<<"Start Capturing..."<<endl;
+            _capture = true;
+        }
+        else if(keyboard_event == 'e')
+        {
+            cout<<"End Capturing..."<<endl;
+            _capture = false;
+        }
 
         display_next_frame();
 
